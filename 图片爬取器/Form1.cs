36c0d6b9e043d64f3dafad5cfe0f9844d2e329dc
yang9035 <yang9035@126.com>
2020-10-22 11:32:46 +0800

    添加项目文件。

---
+++ 图片爬取器/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 图片爬取器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string GetNewURL(string str,int num)
+        {
+            
+            return str;
+        }
+    }
+}
