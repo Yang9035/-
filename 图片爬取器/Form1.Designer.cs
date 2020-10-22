d812c9547a1e7ef4a671308e7a54acf85cd02944
yang9035 <yang9035@126.com>
2020-10-22 17:12:54 +0800

    sssss

--- 图片爬取器/Form1.Designer.cs
+++ 图片爬取器/Form1.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.txtURL = new System.Windows.Forms.TextBox();
-            this.txtSTR = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtStrNumber = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtRegex = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtPictrueHouzhui = new System.Windows.Forms.TextBox();
@@ -41,63 +37,31 @@
             this.lbInfo = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtURL
             // 
-            this.txtURL.Location = new System.Drawing.Point(120, 37);
+            this.txtURL.Location = new System.Drawing.Point(119, 37);
             this.txtURL.Name = "txtURL";
             this.txtURL.Size = new System.Drawing.Size(657, 21);
             this.txtURL.TabIndex = 1;
-            // 
-            // txtSTR
-            // 
-            this.txtSTR.Location = new System.Drawing.Point(120, 76);
-            this.txtSTR.Name = "txtSTR";
-            this.txtSTR.Size = new System.Drawing.Size(131, 21);
-            this.txtSTR.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(267, 80);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 12);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "说明：URL中需要变化的部分";
-            // 
-            // txtStrNumber
-            // 
-            this.txtStrNumber.Location = new System.Drawing.Point(554, 75);
-            this.txtStrNumber.Name = "txtStrNumber";
-            this.txtStrNumber.Size = new System.Drawing.Size(56, 21);
-            this.txtStrNumber.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(622, 80);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 12);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "说明：在URL第几个字符开始";
+            this.txtURL.Text = "https://tieba.baidu.com/p/2460150866?pn=3";
             // 
             // txtRegex
             // 
-            this.txtRegex.Location = new System.Drawing.Point(121, 124);
+            this.txtRegex.Location = new System.Drawing.Point(119, 76);
             this.txtRegex.Name = "txtRegex";
             this.txtRegex.Size = new System.Drawing.Size(394, 21);
             this.txtRegex.TabIndex = 1;
+            this.txtRegex.Text = "src=\\\"(.+?\\\\.jpg)\\\" pic_ext";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(552, 126);
+            this.label3.Location = new System.Drawing.Point(119, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(137, 12);
             this.label3.TabIndex = 5;
@@ -105,22 +69,24 @@
             // 
             // txtPictrueHouzhui
             // 
-            this.txtPictrueHouzhui.Location = new System.Drawing.Point(120, 177);
+            this.txtPictrueHouzhui.Location = new System.Drawing.Point(119, 119);
             this.txtPictrueHouzhui.Name = "txtPictrueHouzhui";
             this.txtPictrueHouzhui.Size = new System.Drawing.Size(88, 21);
             this.txtPictrueHouzhui.TabIndex = 1;
+            this.txtPictrueHouzhui.Text = ".jpg";
             // 
             // txtPictruePath
             // 
-            this.txtPictruePath.Location = new System.Drawing.Point(122, 228);
+            this.txtPictruePath.Location = new System.Drawing.Point(119, 156);
             this.txtPictruePath.Name = "txtPictruePath";
             this.txtPictruePath.Size = new System.Drawing.Size(394, 21);
             this.txtPictruePath.TabIndex = 1;
+            this.txtPictruePath.Text = "F:\\pictrues";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(49, 278);
+            this.label4.Location = new System.Drawing.Point(49, 228);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 12);
             this.label4.TabIndex = 7;
@@ -129,7 +95,7 @@
             // lbInfo
             // 
             this.lbInfo.AutoSize = true;
-            this.lbInfo.Location = new System.Drawing.Point(120, 278);
+            this.lbInfo.Location = new System.Drawing.Point(120, 228);
             this.lbInfo.Name = "lbInfo";
             this.lbInfo.Size = new System.Drawing.Size(41, 12);
             this.lbInfo.TabIndex = 7;
@@ -138,7 +104,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(49, 127);
+            this.label5.Location = new System.Drawing.Point(49, 79);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 12);
             this.label5.TabIndex = 8;
@@ -153,19 +119,10 @@
             this.label6.TabIndex = 9;
             this.label6.Text = "URL";
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(61, 80);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 12);
-            this.label7.TabIndex = 10;
-            this.label7.Text = "循环变量";
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(49, 186);
+            this.label8.Location = new System.Drawing.Point(49, 128);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(65, 12);
             this.label8.TabIndex = 11;
@@ -174,49 +131,35 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(37, 231);
+            this.label9.Location = new System.Drawing.Point(37, 159);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(77, 12);
             this.label9.TabIndex = 12;
             this.label9.Text = "图片存放路径";
             // 
-            // label10
+            // btnStart
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(500, 78);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(29, 12);
-            this.label10.TabIndex = 10;
-            this.label10.Text = "起点";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(624, 228);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 37);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnStart.Location = new System.Drawing.Point(624, 228);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(142, 37);
+            this.btnStart.TabIndex = 13;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 302);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(817, 302);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.lbInfo);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtStrNumber);
-            this.Controls.Add(this.txtSTR);
             this.Controls.Add(this.txtPictrueHouzhui);
             this.Controls.Add(this.txtPictruePath);
             this.Controls.Add(this.txtRegex);
@@ -232,10 +175,6 @@
 
         #endregion
         private System.Windows.Forms.TextBox txtURL;
-        private System.Windows.Forms.TextBox txtSTR;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtStrNumber;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtRegex;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtPictrueHouzhui;
@@ -244,11 +183,9 @@
         private System.Windows.Forms.Label lbInfo;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- 图片爬取器/Form1.cs
+++ 图片爬取器/Form1.cs
@@ -6,8 +6,10 @@ using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace 图片爬取器
 {
@@ -17,13 +19,45 @@ namespace 图片爬取器
         {
             InitializeComponent();
         }
-        public string GetNewURL(string str,int num)
+        public WebClient wc = new WebClient();
+        //得到HTML文本
+        public string GetHtmlText(string url)
         {
-            string newstr = "";
-            string str_1 = str.Substring(0, num);
-            string str_2 = str.Substring(num);
-            newstr = str_1 + "||****||" + str_2;
-            return newstr;
+            wc.Encoding = Encoding.UTF8;
+            string html;
+            html = wc.DownloadString(url);
+            return html;
+        }
+        //得到图片地址集合
+        public MatchCollection GetPictruePath(string html,string strregex)
+        {
+            MatchCollection matches = Regex.Matches(html, strregex);
+            return matches;
+        }
+        //下载图片
+        public void DownLoadPictrue(MatchCollection matches,string pictruetype,string savepath)
+        {
+            int i = 1;
+            foreach (Match item in matches)
+            {
+                Thread.Sleep(100);
+                string str = pictruetype;//图片后缀名称
+                lbInfo.Text="正在下载..." + item.Groups[1].Value;
+                wc.DownloadFile(item.Groups[1].Value, savepath+ i + str);
+                lbInfo.Text = string.Format("{0}下载完毕，准备下一张..", i + str);
+                i++;
+            }
+        }
+
+        //开始爬取
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string txt = txtRegex.Text;
+            lbInfo.Text = txt;
+            //string url = txtURL.Text;
+            //string Htmltxt = GetHtmlText(url);
+            //MatchCollection matches = GetPictruePath(Htmltxt, @txtRegex.Text);
+            //DownLoadPictrue(matches, txtPictrueHouzhui.Text, txtPictruePath.Text);
         }
     }
 }
